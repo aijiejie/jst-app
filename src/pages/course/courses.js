5af5b44aa5513d1bdbd35aad9087283fd824de36
shaohui <125361152@qq.com>
2019-11-06 10:45:16 +0800

    基本框架

---
+++ src/pages/course/courses.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {} from "react-router-dom"
+
+
+export default class Courses extends React.Component{
+
+    render() {
+
+        return <div>
+            Courses
+        </div>
+    }
+}
